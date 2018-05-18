c0babe0effc23fda78792473563e66bcafb6c9b9
hxia200 <hxia3@ncsu.edu>
2018-05-18 18:44:21 -0400

    rearrange project structure and remove unecessary file

--- src/App.vue
+++ src/App.vue
@@ -5,24 +5,6 @@
   </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld'
+<script src='./app.js'></script>
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style src="./app.css"></style>
